25b94e6f51394487f91c017818fe79d7e6d851da
0xkydo <kydo0x@gmail.com>
2022-11-14 19:32:15 -0800

    missing commit file

--- src/DCA.sol
+++ src/DCA.sol
@@ -11,17 +11,19 @@ import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 contract DCA is Ownable {
 
     // Public Variables
-    IERC20 public baseAsset;
+    address public constant USDC;
+    address public constant WETH9;
+    ISwapRouter public immutable swapRouter = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
     uint256 public amount;
-    address public router;
-
+    uint24 public constant poolFee = 500;
+    
     // Private Variables
     address payable private _ledger;
     
     // Events
     event Swap(
-        uint40 amountIn,
-        uint40 amountOut
+        uint256 amountIn,
+        uint256 amountOut
     );
 
     /*
@@ -30,21 +32,49 @@ contract DCA is Ownable {
     constructor(
         uint256 _amount,
         address _router,
-        address _baseAsset,
-        address payable ledger
+        address _USDC,
+        address _WETH9,
+        address payable cold
     ){
-        router = _router;
-        baseAsset = IERC20(_baseAsset);
+        swapRouter = ISwapRouter(_router);
+        USDC = _USDC;
+        WETH9 = _WETH9;
         amount = _amount;
-        _ledger = ledger;
+        _ledger = cold;
+    }
+
+    function approve() public onlyOwner{
+        // Approve the router to spend USDC.
+        TransferHelper.safeApprove(USDC, address(swapRouter), _amount);
     }
 
     /*
-    * @notice swap asset from
+    * @notice swap asset from USDC to WETH
+    * @param amountMin The minimum amount of WETH output. Need to be calculated offchain
     */
-    function swap() public payable onlyOwner {
+    function swap(uint256 amountMin) public payable onlyOwner {
 
-        
+        // Transfer in base asset.
+        baseAsset.transferFrom(_ledger, address(this), _amount);
+
+        // Execute Swap
+        // https://docs.uniswap.org/protocol/guides/swaps/single-swaps
+        ISwapRouter.ExactInputSingleParams memory params =
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: USDC,
+                tokenOut: WETH9,
+                fee: poolFee,
+                recipient: _ledger,
+                deadline: block.timestamp,
+                amountIn: _amount,
+                amountOutMinimum: amountMin,
+                sqrtPriceLimitX96: 0
+            });
+
+        amountOut = swapRouter.exactInputSingle(params);
+
+        emit Swap(_amount ,amountOut);
+
     }
 
     /*
